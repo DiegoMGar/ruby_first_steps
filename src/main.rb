ca56d5288e306e3d52819e2ffe91bf71c4bf9705
Diego Maroto <diego.diemg@gmail.com>
2020-06-26 18:32:28 +0200

    Functions, returns and params

--- src/main.rb
+++ src/main.rb
@@ -8,7 +8,11 @@ puts arr
 puts arr[1]
 
 dict = { k1: 1, k2: 2, k3: 3, k4: 25, k5: 5 }
-puts dict
+puts '===='
+dict.each do |k, v|
+  print "#{k}:#{v} "
+end
+puts "\n===="
 puts dict[:k1]
 puts dict['k2']
 puts :k1 == 'k1'
@@ -21,8 +25,20 @@ puts dict.key(25)
 puts(5.times.collect { true })
 puts(('true ' * 5).strip + '.')
 
-5.times do |i|
+times = 5
+times.times do |i|
   print i
 end
+print "\n"
+def sum(num1, num2)
+  puts 'Sum is: ' + (num1 + num2).to_s
+  num1 + num2
+end
+
+sum(1, 2)
+sum 2, 5
+var1 = 7
+var2 = 3
+puts 'Returned is ' + sum(var1, var2).to_s
 
 puts "\n== end =="
